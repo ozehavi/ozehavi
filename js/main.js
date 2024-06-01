938dab78041f2a73057f15131970ab7e177a2d8b
orenzehavi <oren.zehavi@methoda.com>
2024-06-01 17:11:17 +0300

    in progress

--- js/main.js
+++ js/main.js
@@ -12,14 +12,11 @@ $(window).on('scroll', function () {
 	}
 });
 
-
-
-
-
 $(document).ready(function(){
 
 // mobile_menu
 var menu = $('ul#navigation');
+console.log("menu", menu)
 if(menu.length){
 	menu.slicknav({
 		prependTo: ".mobile_menu",
@@ -27,10 +24,13 @@ if(menu.length){
 		openedSymbol:'-'
 	});
 };
-// blog-menu
-  // $('ul#blog-menu').slicknav({
-  //   prependTo: ".blog_menu"
-  // });
+
+$('.slicknav_nav a').on('click', function(){
+  console.log("click")
+    menu.slicknav('close');
+
+});
+
 
 // review-active
 $('.slider_active').owlCarousel({
@@ -68,42 +68,6 @@ autoplaySpeed: 800,
 });
 
 // review-active
-$('.testmonial_active ').owlCarousel({
-  loop:true,
-  margin:0,
-items:1,
-autoplay:true,
-navText:['<i class="ti-angle-left"></i>','<i class="ti-angle-right"></i>'],
-  nav:true,
-dots:false,
-autoplayHoverPause: true,
-autoplaySpeed: 800,
-  responsive:{
-      0:{
-          items:1,
-          dots:false,
-          nav:false,
-      },
-      767:{
-          items:1,
-          dots:false,
-          nav:false,
-      },
-      992:{
-          items:1,
-          nav:false
-      },
-      1200:{
-          items:1,
-          nav:false
-      },
-      1500:{
-          items:1
-      }
-  }
-});
-
-// review-active
 $('.financial_active').owlCarousel({
   loop:true,
   margin:0,
@@ -115,40 +79,6 @@ dots:false,
 autoplayHoverPause: true,
 autoplaySpeed: 800,
 
-  responsive:{
-      0:{
-          items:1,
-          nav:false
-      },
-      767:{
-          items:1,
-          nav:false
-      },
-      992:{
-          items:1
-      },
-      1200:{
-          items:1
-      },
-      1500:{
-          items:1
-      }
-  }
-});
-
-// review-active
-$('.testmonial_active2').owlCarousel({
-  loop:true,
-  margin:0,
-items:1,
-autoplay:false,
-navText:['<i class="ti-angle-left"></i>','<i class="ti-angle-right"></i>'],
-  nav:false,
-dots:true,
-autoplayHoverPause: true,
-autoplaySpeed: 800,
-// dotsData: true,
-center: true,
   responsive:{
       0:{
           items:1,
